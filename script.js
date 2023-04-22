5401c37bf738a168a9c4d49dbd6058e828515637
PlsDonateHelp <124628518+PlsDonateHelp@users.noreply.github.com>
2023-04-22 09:10:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const textInput = document.getElementById("textInput");
-const fontInput = document.getElementById("fontInput");
-const sizeInput = document.getElementById("sizeInput");
-const colorInput = document.getElementById("colorInput");
-const strokeColorInput = document.getElementById("strokeColorInput");
-const thicknessInput = document.getElementById("thicknessInput");
-const preview = document.getElementById("preview");
-const exportButton = document.getElementById("exportButton");
+// Get references to the input box, output text, and copy button
+const inputBox = document.getElementById('inputBox');
+const outputText = document.getElementById('outputText');
+const copyButton = document.getElementById('copyButton');
 
-const updatePreview = () => {
-  preview.innerHTML = textInput.value;
-  preview.style.fontFamily = fontInput.value;
-  preview.style.fontSize = `${sizeInput.value}px`;
-  preview.style.color = colorInput.value;
-  preview.style.textShadow = `${thicknessInput.value}px ${thicknessInput.value}px 0 ${strokeColorInput.value}`;
-};
+// Add an event listener to the input box to listen for changes
+inputBox.addEventListener('input', () => {
+  // Set the output text to the value of the input box
+  outputText.textContent = inputBox.value;
 
-textInput.addEventListener("input", updatePreview);
-fontInput.addEventListener("change", updatePreview);
-sizeInput.addEventListener("input", updatePreview);
-colorInput.addEventListener("input", updatePreview);
-strokeColorInput.addEventListener("input", updatePreview);
-thicknessInput.addEventListener("input", updatePreview);
-
-exportButton.addEventListener("click", () => {
-  domtoimage.toBlob(preview)
-    .then(function (blob) {
-      saveAs(blob, "image.png");
-    });
+  // Check if the input is 'Olix' and redirect if true
+  if (inputBox.value.toLowerCase() === 'olix') {
+    window.location.href = 'https://plsdonatehelp.github.io/olix';
+  }
 });
 
-updatePreview();
+// Add an event listener to the copy button to listen for clicks
+copyButton.addEventListener('click', () => {
+  // Create a new textarea element to hold the output text
+  const textarea = document.createElement('textarea');
+  textarea.value =
+    '<stroke color="#2A0030" thickness="4"><font size="90"><font color="#FF0000"><font face="LuckiestGuy">' +
+    inputBox.value +
+    '</font></font></font></stroke>';
+  document.body.appendChild(textarea);
+
+  // Select the text in the textarea and copy it
+  textarea.select();
+  document.execCommand('copy');
+
+  // Remove the textarea from the DOM
+  document.body.removeChild(textarea);
+});
