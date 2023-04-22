cfa279f407dd37fa96250b115aafbb944fe110df
PlsDonateHelp <124628518+PlsDonateHelp@users.noreply.github.com>
2023-04-22 09:16:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-// Get references to the input box, output text, and copy button
-const inputBox = document.getElementById('inputBox');
-const outputText = document.getElementById('outputText');
-const copyButton = document.getElementById('copyButton');
+const inputBox = document.getElementById("inputBox");
+      const outputText = document.getElementById("outputText");
+      const copyButton = document.getElementById("copyButton");
 
-// Add an event listener to the input box to listen for changes
-inputBox.addEventListener('input', () => {
-  // Set the output text to the value of the input box
-  outputText.textContent = inputBox.value;
+      inputBox.addEventListener("input", () => {
+        const text = inputBox.value.toLowerCase();
+        let output = "";
 
-  // Check if the input is 'Olix' and redirect if true
-  if (inputBox.value.toLowerCase() === 'olix') {
-    window.location.href = 'https://plsdonatehelp.github.io/olix';
-  }
-});
+        if (text === "yes") {
+          output =
+            '<stroke color="#2A0030" thickness="4"><font size="90"><font color="#FF0000"><font face="LuckiestGuy">no</font></font></font></stroke>';
+        } else {
+          output = `<stroke color="#2A0030" thickness="4"><font size="90"><font color="#FF0000"><font face="LuckiestGuy">${text}</font></font></font></stroke>`;
+        }
 
-// Add an event listener to the copy button to listen for clicks
-copyButton.addEventListener('click', () => {
-  // Create a new textarea element to hold the output text
-  const textarea = document.createElement('textarea');
-  textarea.value =
-    '<stroke color="#2A0030" thickness="4"><font size="90"><font color="#FF0000"><font face="LuckiestGuy">' +
-    inputBox.value +
-    '</font></font></font></stroke>';
-  document.body.appendChild(textarea);
+        outputText.innerHTML = output;
+      });
 
-  // Select the text in the textarea and copy it
-  textarea.select();
-  document.execCommand('copy');
+      copyButton.addEventListener("click", () => {
+        const textarea = document.createElement("textarea");
+        textarea.value = outputText.innerHTML;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+      });
 
-  // Remove the textarea from the DOM
-  document.body.removeChild(textarea);
-});
+      if (inputBox.value.toLowerCase() === "olix") {
+        window.location.href = "https://plsdonatehelp.github.io/olix";
+      }
